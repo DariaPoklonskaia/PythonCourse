d71e7f5bafed5c617fa3e8f7907bd967af434e2c
Daria <skotnikovada@mail.ru>
2022-09-16 22:57:28 +0300

    sem4 is done

--- seminar4.py
+++ seminar4.py
@@ -153,14 +153,63 @@
 
 #Ex7. Задайте строку из набора чисел. Напишите программу, которая покажет большее и меньшее число. В качестве символа-разделителя используйте пробел
 
-numbers = str(input('enter numbers and use space as separator : '))
-list1 = []
+# numbers = str(input('enter numbers and use space as separator : '))
+# list1 = []
 
-for i in numbers:
-    if i != ' ':
-        num = int(i)
-        list1.append(num)
-print(list1)
+# for i in numbers:
+#     if i != ' ':
+#         num = int(i)
+#         list1.append(num)
+# print(list1)
+
+# # def FIndMax(any_list):
+# #     for i in range(0, len(any_list)):
+# #         maxV = any_list[0]
+# #         if any_list[i] > maxV:
+# #             maxV= any_list[i]
+# #     return maxV
+
+# # def FIndMin(any_list):
+# #     for i in range(0, len(any_list)):
+# #         minV = any_list[0]
+# #         if any_list[i] < minV:
+# #             minV = any_list[i]
+# #     return minV
+# # print(FIndMin(list1))
+# # print(FIndMax(list1))
+
+# print(f'[{min(list1)} {max(list1)}]')
+
+#Ex7.Var2.
+
+# str = '12 32 0 15'
+# lst = [int(i) for i in str.split()]
+# print(min(lst))
+# print(max(lst))
 
 #Ex8. Найдите корни квадратного уравнения Ax² + Bx + C = 0 двумя способами: 1) с помощью математических формул нахождения корней квадратного уравнения
 # 2) с помощью дополнительных библиотек Python
+
+def discriminant(a: float, b: float, c: float) -> float:
+    return b ** 2 - 4 * a * c
+
+#var2 Ex2
+def solve_quadratic(a: float, b: float, c: float) -> str:
+    if a == 0:
+        raise ValueError("Not quadratic equation")
+
+    d = discriminant(a, b, c)
+    if d < 0:
+        return "No roots"
+    elif d == 0:
+        return f"One root x = {-b / (2 * a)}"
+    else:
+        x1 = (-b + d ** 0.5) / (2 * a)
+        x2 = (-b - d ** 0.5) / (2 * a)
+        return f"Two roots: x1 = {x1}, x2 = {x2}"
+
+if __name__ == "__main__":
+    print(solve_quadratic(5, -9, -2))
+    print(solve_quadratic(1, -4, 4))
+    print(solve_quadratic(1, -5, 9))
+    print(solve_quadratic(0, 1, 2))
